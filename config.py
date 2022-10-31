dc097c8a297a2509c5cf68a20c7e8c4d52168547
Satyendra Niranjan <satyendra.niranjan123@gmail.com>
2022-10-31 18:48:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'test'
+app.config['MYSQL_DATABASE_USER'] = 'test1'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Adminuser123#'
 app.config['MYSQL_DATABASE_DB'] = 'flask'
-app.config['MYSQL_DATABASE_HOST'] = '10.0.0.6'
+app.config['MYSQL_DATABASE_HOST'] = '4.224.18.242'
 mysql.init_app(app)
 
